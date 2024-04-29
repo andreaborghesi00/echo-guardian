6210ff15910018a8c0b7f24e5ceb8bb44f6aff92
andypalmi <andypalmi@libero.it>
2024-04-29 10:17:04 +0200

    script

---
+++ A02.py
@@ -0,0 +1,64 @@
+# %% [markdown]
+# # Detection of breast cancers
+# Train model on CT and PET scans to predict malignant or benign cancer by validating it with histological positive or negative labels
+# - Radiologist accuracy: 80% -> Get an accuracy higher than 80%
+# ### What to do
+# - Anatomical location of the region
+# - Characterization of the region
+# - Therapeutic choice
+# - Monitoring of the therapy to monitor patient's response to treatment
+# ### Risk Assessment Analysis -> High Risk tools
+# Include Risk Assessment Analysis of the tool in pitch deck
+# - Include list of requirements of our system: operating system, tools, forms, etc, cybersecurity risks (login)
+# - Tool performances in terms of
+#     - Sensitivity (TP rate) -> **at least 90%**
+#     - FP rate
+#     - Time
+# ### Model testing during presentation
+# **1 second as maximum response time for testing** -> model will be tested live during presentation
+# ### Data privacy
+# - Require auth
+# - No personal info (name, surname, TAX ID code, address, etc)
+# - Cluster ages to reduce privacy risk (each 5 years?)
+# ### Requirements
+# - DLL library to call functions from -> **segregation of functions**
+# - Must have **EXIT** button
+# - Cybersecurity of the tool
+# - AUC greather than 0.85
+# - Sensitivity higher than 0.90
+# 
+# ### Ethics -> **INCLUDE IT**
+# Simulate that we actually got these documents in the first few slides
+# - Informed consent (got from the patient)
+# - Ethic Committee Approval (about 2 months after study request)
+#     - It was approved
+# - Anonimization
+#     - Define the measures in the data
+# 
+# ### Methods to be used:
+# - Radiomics library
+# - Random Forest or SVM (choose one, maybe PCA to choose the best features)
+# - CNNs
+# 
+# ### Dataset (with masks)
+# - 437 benign
+# - 210 malignant
+
+# %%
+import nibabel as nib
+import matplotlib.pyplot as plt ## ciao
+
+# %%
+path = 'datasets/code__esempi/data/multiple-patients/MRI/mwp1-mri-1.nii'
+img = nib.load(path).get_fdata()
+img.shape
+# ayoooo ## Andrea
+
+# %%
+plt.style.use('default')
+fig, axes = plt.subplots(4,4, figsize=(12,12))
+for i, ax in enumerate(axes.reshape(-1)):
+    ax.imshow(img[:,:,1 + i])
+plt.show() # ciao 
+
+print('ciao')
