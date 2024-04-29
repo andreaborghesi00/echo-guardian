16b1701afcc0b86c6beb007a5347bb00b2b9e255
Mirko Morello <mirkomorello97@gmail.com>
2024-04-29 12:23:55 +0200

    cleared stuff

--- A02.py
+++ A02.py
@@ -58,22 +58,6 @@ import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 
-# %% [Plot MRI]
-path = 'code__esempi/data/multiple-patients/MRI/mwp1-mri-1.nii'
-img = nib.load(path).get_fdata()
-img.shape
-plt.style.use('default')
-fig, axes = plt.subplots(4,10, figsize=(12,12))
-for i, ax in enumerate(axes.reshape(-1)):
-    ax.imshow(img[:, i + 20, :])
-plt.show()
-
-#%% [Plot PET]
-path = 'code__esempi/data/single-patient/PET/dicom/PET-001.dcm'
-pet = pydicom.dcmread(path).pixel_array
-plt.imshow(pet, cmap='viridis')
-plt.show()
-
 # %% 
 
 path = 'dataset/'
@@ -87,66 +71,5 @@ for folder in os.listdir(path):
                                         'label': [0 if 'benign' in file else 1]})])
 df.index = range(1, len(df) + 1)
 df
-    
-# %%
-df
-# %%
-#%% [Plot CT]
-path = 'code__esempi/data/single-patient/CT/dicom/*.dcm'
 
-# load the DICOM files
-files = []
-
-for fname in glob.glob(path, recursive=False):
-    print("loading: {}".format(fname))
-    files.append(pydicom.dcmread(fname))
-
-print("file count: {}".format(len(files)))
-
-# skip files with no SliceLocation (eg scout views)
-slices = []
-skipcount = 0
-for f in files:
-    if hasattr(f, 'SliceLocation'):
-        slices.append(f)
-    else:
-        skipcount = skipcount + 1
-
-print("skipped, no SliceLocation: {}".format(skipcount))
-
-# ensure they are in the correct order
-slices = sorted(slices, key=lambda s: s.SliceLocation)
-
-# pixel aspects, assuming all slices are the same
-print(type(slices[0]))
-ps = slices[0].PixelSpacing
-ss = slices[0].SliceThickness
-ax_aspect = ps[1]/ps[0]
-sag_aspect = ps[1]/ss
-cor_aspect = ss/ps[0]
-
-# create 3D array
-img_shape = list(slices[0].pixel_array.shape)
-img_shape.append(len(slices))
-img3d = np.zeros(img_shape)
-
-# fill 3D array with the images from the files
-for i, s in enumerate(slices):
-    img2d = s.pixel_array
-    img3d[:, :, i] = img2d
-
-# plot 3 orthogonal slices
-a1 = plt.subplot(2, 2, 1)
-plt.imshow(img3d[:, :, img_shape[2]//2], cmap='gray')
-a1.set_aspect(ax_aspect)
-
-a2 = plt.subplot(2, 2, 2)
-plt.imshow(img3d[:, img_shape[1]//2, :], cmap='gray')
-a2.set_aspect(sag_aspect)
-
-a3 = plt.subplot(2, 2, 3)
-plt.imshow(img3d[img_shape[0]//2, :, :].T, cmap='gray', origin="lower")
-a3.set_aspect(cor_aspect)
-
-plt.show()
 # %%
