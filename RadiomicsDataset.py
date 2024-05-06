88914a9ff1e60b30d6e1da32b4bf6cfecaa5cf27
coso <00borghesiandrea@gmail.com>
2024-05-06 11:52:14 +0200

    scaling data

--- RadiomicsDataset.py
+++ RadiomicsDataset.py
@@ -3,9 +3,11 @@ import SimpleITK as sitk
 import radiomics
 import torch
 import numpy as np
+from tqdm import tqdm
+from sklearn.utils.validation import check_is_fitted
 
 class RadiomicsDataset(Dataset):
-    def __init__(self, img_mask_paths, labels):
+    def __init__(self, img_mask_paths, labels, scaler=None):
         glcm_feats = [ # i know it's annoying, and it took way too long to find, but this is how you exclude a feature from the extraction
             'Autocorrelation',
             'ClusterProminence',
@@ -43,20 +45,30 @@ class RadiomicsDataset(Dataset):
         self.extractor.enableFeatureClassByName('glszm')
         self.extractor.enableFeatureClassByName('ngtdm')
 
+        self.scaler = scaler
         self.length = len(labels)
         self.labels = torch.tensor(labels.tolist())
         self.labels = self.labels.unsqueeze(1).float()
         self.img_mask_paths = img_mask_paths
         
         self.rad_features = []
-        for i in range(self.length):
+        for i in tqdm(range(self.length)):
             image = sitk.ReadImage(self.img_mask_paths[i][0], sitk.sitkInt32)
             mask = sitk.ReadImage(self.img_mask_paths[i][1], sitk.sitkInt32)
             features = self.extractor.execute(image, mask, voxelBased=False, label=255)
-            features_values = torch.tensor([float(features[key]) for key in features if key.startswith('original_')])
+            features_values = [float(features[key]) for key in features if key.startswith('original_')]
             self.rad_features.append(features_values)
+
+        try: 
+            self.rad_features = self.scaler.transform(np.array(self.rad_features))        
+        except:
+            print('Fitting scaler')
+            self.scaler = self.scaler.fit(np.array(self.rad_features))
+            self.rad_features = self.scaler.transform(np.array(self.rad_features))        
+
+        self.rad_features = torch.tensor(self.rad_features).float()        
+
         
-                
     def __len__(self):
         return self.length
     
