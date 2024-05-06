45277f56d9c717d4a7a6e98915db2e8043fdb4e5
coso <00borghesiandrea@gmail.com>
2024-05-06 09:24:42 +0200

    updated dataset, removed unused features, simple NN

--- RadiomicsDataset.py
+++ RadiomicsDataset.py
@@ -1,24 +1,58 @@
 from torch.utils.data import Dataset
 import SimpleITK as sitk
 import radiomics
+import torch
 
 class RadiomicsDataset(Dataset):
-    def __init__(self, df):
-        self.df = df
-        self.extractor = radiomics.featureextractor.RadiomicsFeatureExtractor()
-        self.extractor.enableAllFeatures()
-        self.length = len(df)
+    def __init__(self, img_mask_paths, labels):
+        glcm_feats = [ # i know it's annoying, and it took way too long to find, but this is how you exclude a feature from the extraction
+            'Autocorrelation',
+            'ClusterProminence',
+            'ClusterShade',
+            'ClusterTendency',
+            'Contrast',
+            'Correlation',
+            'DifferenceAverage',
+            'DifferenceEntropy',
+            'DifferenceVariance',
+            'Id',
+            'Idm',
+            'Idmn',
+            'Idn',
+            'Imc1',
+            'Imc2',
+            'InverseVariance',
+            'JointAverage',
+            'JointEnergy',
+            'JointEntropy',
+            'MCC',
+            'MaximumProbability',
+            # 'SumAverage',
+            'SumEntropy',
+            'SumSquares'
+        ]
 
-        self.image_paths = df['image'].values
-        self.mask_paths = df['mask'].values
-        self.labels = df['label'].values
+        self.extractor = radiomics.featureextractor.RadiomicsFeatureExtractor()
+        self.extractor.disableAllFeatures()
+        self.extractor.enableFeatureClassByName('firstorder')
+        self.extractor.enableFeatureClassByName('shape2D')
+        self.extractor.enableFeaturesByName(glcm=glcm_feats)
+        self.extractor.enableFeatureClassByName('gldm')
+        self.extractor.enableFeatureClassByName('glrlm')
+        self.extractor.enableFeatureClassByName('glszm')
+        self.extractor.enableFeatureClassByName('ngtdm')
+
+        self.length = len(labels)
+        self.labels = torch.tensor(labels.tolist())
+        self.img_mask_paths = img_mask_paths
 
     def __len__(self):
         return self.length
     
-
     def __getitem__(self, idx):
-        image = sitk.ReadImage(self.image_paths[idx], sitk.sitkInt32)
-        mask = sitk.ReadImage(self.mask_paths[idx], sitk.sitkInt32)
+        image = sitk.ReadImage(self.img_mask_paths[idx][0], sitk.sitkInt32)
+        mask = sitk.ReadImage(self.img_mask_paths[idx][1], sitk.sitkInt32)
         features = self.extractor.execute(image, mask, voxelBased=False, label=255)
-        return features, self.labels[idx]
+        features_values = torch.tensor([float(features[key]) for key in features if key.startswith('original_')]) # che coglioni, che famo preprocessiamo tutte le immagini?
+
+        return features_values, self.labels[idx]
