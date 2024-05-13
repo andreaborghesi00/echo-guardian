47e1be55c0b5af5e9d8c72bf76672c57f9419cb2
andypalmi <andypalmi@libero.it>
2024-05-13 13:51:41 +0200

    gui-dash updated

--- dataset/gui-dash.py
+++ dataset/gui-dash.py
@@ -1,66 +1,64 @@
+import numpy as np
 from dash import Dash, dcc, html, Input, Output, no_update, callback
 import plotly.express as px
 import dash_daq as daq
-from skimage import data
+from skimage import data, draw
+from scipy import ndimage
 
+def path_to_indices(path):
+    """From SVG path to numpy array of coordinates, each row being a (row, col) point
+    """
+    indices_str = [
+        el.replace("M", "").replace("Z", "").split(",") for el in path.split("L")
+    ]
+    return np.rint(np.array(indices_str, dtype=float)).astype(int)
 
-@callback(
-    Output("graph-styled-annotations", "figure"),
-    Input("opacity-slider", "value"),
-    prevent_initial_call=True,
-)
-def on_style_change(slider_value):
-    fig = px.imshow(img)
-    fig.update_layout(
-        dragmode="drawclosedpath",
-        newshape=dict(opacity=slider_value, fillcolor='#808080'),
-    )
-    return fig
-
-
+def path_to_mask(path, shape):
+    """From SVG path to a boolean array where all pixels enclosed by the path
+    are True, and the other pixels are False.
+    """
+    cols, rows = path_to_indices(path).T
+    rr, cc = draw.polygon(rows, cols)
+    mask = np.zeros(shape, dtype=bool)
+    mask[rr, cc] = True
+    mask = ndimage.binary_fill_holes(mask)
+    return mask
 
 img = data.chelsea()
+mask = np.zeros(img.shape, dtype=bool)
 fig = px.imshow(img)
-fig.update_layout(
-    dragmode="drawclosedpath",
-    newshape=dict(fillcolor="cyan", opacity=0.3, line=dict(color="darkblue", width=8)),
-)
+fig_mask = px.imshow(mask)
+fig.update_layout(dragmode='drawclosedpath')
+
 
 app = Dash(__name__)
 app.layout = html.Div(
     [
-        html.H3("Drag and draw annotations"),
-        dcc.Graph(id="graph-styled-annotations", figure=fig),
-        html.Pre('Opacity of annotations'),
-        dcc.Slider(id="opacity-slider", min=0, max=1, value=0.5, step=0.1, tooltip={'always_visible':True}),
-        dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files'),
-
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-data-upload'),
+        html.H3("Draw a path to show the histogram of the ROI"),
+        html.Div(
+            [dcc.Graph(id="ultrasound-image", figure=fig),],
+            style={"width": "60%", "display": "inline-block", "padding": "0 0"},
+        ),
+        html.Div(
+            [dcc.Graph(id="mask-image", figure=fig_mask),],
+            style={"width": "40%", "display": "inline-block", "padding": "0 0"},
+        ),
     ]
 )
 
-# app.layout = html.Div([
-#     
-# ])
-
+@callback(
+    Output("mask-image", "figure"),
+    Input("ultrasound-image", "relayoutData"),
+    prevent_initial_call=True,
+)
+def on_new_annotation(relayout_data):
+    if "shapes" in relayout_data:
+        last_shape = relayout_data["shapes"][-1]
+        mask = path_to_mask(last_shape["path"], img.shape)
+        fig_mask = px.imshow(mask)
+        return fig_mask
+    else:
+        return no_update
 
 if __name__ == "__main__":
     app.run(debug=True)
