86af5cc3d4d8df6e7d454ad90681626ba58fa5f9
Coso <00borghesiandrea@gmail.com>
2024-05-22 17:37:31 +0200

    fixed user roi draw

--- gui-dash.py
+++ gui-dash.py
@@ -41,6 +41,7 @@ def path_to_mask(path, shape):
     mask = np.zeros(shape, dtype=bool)
     mask[rr_clipped, cc_clipped] = True
     mask_numpy = np.array(mask)
+
     mask = ndimage.binary_fill_holes(mask)
     return mask, mask_numpy
 
@@ -175,7 +176,9 @@ app.layout = html.Div([
 def on_new_annotation(relayout_data, image):
     if "shapes" in relayout_data:
         last_shape = relayout_data["shapes"][-1]
-        mask_numpy= path_to_mask(last_shape["path"], np.array(image).shape)
+        mask, mask_numpy= path_to_mask(last_shape["path"], np.array(image).shape)
+        # squeeze the first dimension
+        print(mask_numpy.shape)
         return mask_numpy
     else:
         raise PreventUpdate
@@ -233,8 +236,8 @@ def on_predict(n_clicks_classify, n_clicks_classify_segmenter, image, mask, segm
             return "Please upload an image and draw an ROI before predicting."
         else:
             try:
-                class_pred = predict_class(image, mask)
-                return f"Predicted class: {class_pred}"
+                class_pred = predict_class(image, mask).cpu().numpy()[0][0]
+                return f'Predicted class with user segmentation:\n {f"Malignant {class_pred*100:.2f}%" if np.round(class_pred) == 1 else f"Benign {(1-class_pred)*100:.2f}%"}'
             except Exception as e:
                 return f"Error: {str(e)}"
     elif ctx.triggered_id == "classify-with-segmenter-button":
